c01612f410b9cbce8fce8f273cb68d8a49c2474c
Jatin-sudo <jatin.gawri@s.amity.edu>
2025-07-04 23:49:34 +0530

    INSERT

---
+++ 16 - INSERT INTO/INSERT.sql
@@ -0,0 +1,21 @@
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (1, 'Twitter', 'San Francisco 🌁', 2006);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (2, 'Duolingo', 'Pittsburgh 🐝', 2011);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (3, 'BeReal', 'Paris 🇫🇷', 2020);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (4, 'Codedex', 'New York 🗽', 2022);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (5, 'Github', 'San Francisco🌁', 2008);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (6, 'Apple', 'Cuppertino', 1976);
+
+
+SELECT *
+FROM companies;
